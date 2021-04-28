ec5e67b368a09e806fba863ea21a165963da2f85
ltogt <omnesiaorg@gmail.com>
2021-04-28 15:34:17 +0200

    split out ltogt_utils_flutter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.2
+* Split out `ltogt_utils_flutter` to allow for pure dart projects to depend on this project
+
 ## 0.0.1
 
 * Collect utils from multiple of my projects.
--- lib/ltogt_utils.dart
+++ lib/ltogt_utils.dart
@@ -7,9 +7,3 @@ export 'src/list/list_generator.dart';
 export 'src/list/list_extension.dart';
 
 export 'src/num/num_helper.dart';
-
-export 'src/widget/builder/conditional_parent_widget.dart';
-export 'src/widget/future/future_switch.dart';
-export 'src/widget/post_frame/render_helper.dart';
-export 'src/widget/post_frame/post_frame_builder.dart';
-export 'src/widget/responsive/row_or_column.dart';
