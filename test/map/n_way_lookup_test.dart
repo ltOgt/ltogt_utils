4d0f8db6ec0f5333441af28418d21d1d05dff071
ltogt <omnesiaorg@gmail.com>
2021-06-23 13:29:48 +0200

    add generic test not really necessary since covered by other indexable

--- test/map/n_way_lookup_test.dart
+++ test/map/n_way_lookup_test.dart
@@ -57,6 +57,29 @@ void main() {
       expect(m2 == target.get(Method2Response, 2), isTrue);
     });
   });
+
+  group('Generic Indexable', () {
+    test('Should find correct object via every indexed field', () {
+      final m1 = GenericIndexableMapping<Method1Request, Method1Response>(
+        path: "method_one",
+      );
+
+      final m2 = GenericIndexableMapping<Method2Request, Method2Response>(
+        path: "method_two",
+      );
+
+      NLookup<GenericIndexableMapping> target = NLookup.indexable<GenericIndexableMapping>(
+        [m1, m2],
+      );
+
+      expect(m1 == target.get(m1.path, 0), isTrue);
+      expect(m1 == target.get(Method1Request, 1), isTrue);
+      expect(m1 == target.get(Method1Response, 2), isTrue);
+      expect(m2 == target.get(m2.path, 0), isTrue);
+      expect(m2 == target.get(Method2Request, 1), isTrue);
+      expect(m2 == target.get(Method2Response, 2), isTrue);
+    });
+  });
 }
 
 // Example classes ===============================
@@ -97,4 +120,17 @@ class IndexableMapping extends Indexable {
 
   @override
   List<IndexValue> get indexes => [path, requestType, responseType];
+}
+
+class GenericIndexableMapping<REQ extends Request, RES extends Response> extends Indexable {
+  final String path;
+  Type get requestType => REQ;
+  Type get responseType => RES;
+
+  GenericIndexableMapping({
+    required this.path,
+  });
+
+  @override
+  List<IndexValue> get indexes => [path, REQ, RES];
 }
