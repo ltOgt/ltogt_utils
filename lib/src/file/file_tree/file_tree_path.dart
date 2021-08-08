abff42324009baae3b51bd100334411a56278201
ltogt <omnesiaorg@gmail.com>
2021-08-08 19:13:30 +0200

    fix errpr in pathFromFIleTreePathIfValid previously mapped root twice

--- lib/src/file/file_tree/file_tree_path.dart
+++ lib/src/file/file_tree/file_tree_path.dart
@@ -1,8 +1,12 @@
+import 'package:ltogt_utils/src/file/file_tree/file_tree.dart';
+
 /// A path constructed from [FileTree]
 class FileTreePath {
   final List<String> segments;
 
   FileTreePath(this.segments);
+
+  void addSegment(FileTreeEntity e) => this.segments.add(e.name);
 
   List<String> encode() => segments;
 
---
+++ test/file/file_tree_helper_test.dart
@@ -0,0 +1,32 @@
+// Import the test package and Counter class
+import 'dart:io';
+
+import 'package:ltogt_utils/src/file/file_helper.dart';
+import 'package:ltogt_utils/src/file/file_tree/file_tree.dart';
+import 'package:ltogt_utils/src/file/file_tree/file_tree_helper.dart';
+import 'package:ltogt_utils/src/file/file_tree/file_tree_path.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('File Tree Helper', () {
+    test('pathFromFileTreePathIfValid', () async {
+      String path = FileTreeHelper.pathFromFileTreePathIfValid(
+        fileTree: FileTree(
+          rootDir: FileTreeDir(
+            name: "root",
+            dirs: [
+              FileTreeDir(
+                name: "child",
+                files: [FileTreeFile(name: "file.txt")],
+              ),
+            ],
+          ),
+        ),
+        rootDir: Directory("root"),
+        fileTreePath: FileTreePath(["root", "child", "file.txt"]),
+      );
+
+      expect(path, equals("root/child/file.txt"));
+    });
+  });
+}
