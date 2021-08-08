9c130a66ed34cc8071ecda2b0d6bea5702f4d8cb
ltogt <omnesiaorg@gmail.com>
2021-08-08 21:49:27 +0200

    expose segments without root

--- lib/src/file/file_tree/file_tree_path.dart
+++ lib/src/file/file_tree/file_tree_path.dart
@@ -8,6 +8,8 @@ class FileTreePath {
 
   void addSegment(FileTreeEntity e) => this.segments.add(e.name);
 
+  List<String> get segmentsWithoutRoot => segments.length < 2 ? const [] : segments.sublist(1);
+
   List<String> encode() => segments;
 
   static FileTreePath decode(List<dynamic> l) => FileTreePath(l.cast());
