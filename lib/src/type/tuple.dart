6b9bff0358f03e04d67b754a66e1282e8deade11
ltOgt <omnesiaorg@gmail.com>
2022-02-18 12:03:39 +0100

    add tuple

---
+++ lib/src/type/tuple.dart
@@ -0,0 +1,27 @@
+class Tuple<A, B> {
+  final A a;
+  final B b;
+
+  const Tuple({
+    required this.a,
+    required this.b,
+  });
+
+  @override
+  String toString() => "Tuple(" + ('a: $a, b: $b') + ")";
+}
+
+class Triple<A, B, C> {
+  final A a;
+  final B b;
+  final B c;
+
+  const Triple({
+    required this.a,
+    required this.b,
+    required this.c,
+  });
+
+  @override
+  String toString() => "Triple(" + ('a: $a, b: $b, c: $c') + ")";
+}
