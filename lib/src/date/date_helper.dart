1ced7851c7ff9c7e8de20d805bca7428133c7f34
ltOgt <omnesiaorg@gmail.com>
2022-03-13 19:06:32 +0100

    add yyyy-mm-dd hh:mm.ss for DateTime.now

--- lib/src/date/date_helper.dart
+++ lib/src/date/date_helper.dart
@@ -1,10 +1,18 @@
 import 'package:ltogt_utils/src/num/num_helper.dart';
 
 class DateHelper {
+  /// prints yyyy-mm-dd
   static String dateString(DateTime date) {
     final yyyy = NumHelper.paddedString(date.year, 9999);
     final mm = NumHelper.paddedString(date.month, 12);
     final dd = NumHelper.paddedString(date.day, 31);
     return "$yyyy-$mm-$dd";
+  }
+
+  /// prints yyyy-mm-dd hh:mm.ss for DateTime.now()
+  static String nowString() {
+    final n = DateTime.now();
+    String _pad2(int i) => NumHelper.paddedString(i, 10);
+    return "${n.year}-${_pad2(n.month)}-${_pad2(n.day)} ${_pad2(n.hour)}:${_pad2(n.minute)}.${_pad2(n.second)}";
   }
 }
