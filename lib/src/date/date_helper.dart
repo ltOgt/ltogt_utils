edd49de8cd3b6301bf0a5d82986f0b0bb4b909f6
ltOgt <omnesiaorg@gmail.com>
2022-04-04 13:27:34 +0200

    add dateTimeString

--- lib/src/date/date_helper.dart
+++ lib/src/date/date_helper.dart
@@ -9,10 +9,15 @@ class DateHelper {
     return "$yyyy-$mm-$dd";
   }
 
-  /// prints yyyy-mm-dd hh:mm.ss for DateTime.now()
-  static String nowString() {
-    final n = DateTime.now();
+  /// prints yyyy-mm-dd hh:mm.ss
+  static String dateTimeString(DateTime date) {
+    final n = date;
     String _pad2(int i) => NumHelper.paddedString(i, 10);
     return "${n.year}-${_pad2(n.month)}-${_pad2(n.day)} ${_pad2(n.hour)}:${_pad2(n.minute)}.${_pad2(n.second)}";
+  }
+
+  /// prints yyyy-mm-dd hh:mm.ss for DateTime.now()
+  static String nowString() {
+    return dateTimeString(DateTime.now());
   }
 }
