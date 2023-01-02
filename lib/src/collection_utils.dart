8a684627e15272906c925031e1787bb7f648ccb9
ltOgt <omnesiaorg@gmail.com>
2023-01-02 09:16:16 +0100

    make static

--- lib/src/collection_utils.dart
+++ lib/src/collection_utils.dart
@@ -1,20 +1,20 @@
 abstract class CollectionUtils {
-  Set<V> setAdd<V>(Set<V> current, V v) => {...current, v};
-  Set<V> setRemove<V>(Set<V> current, V v) => {
+  static Set<V> setAdd<V>(Set<V> current, V v) => {...current, v};
+  static Set<V> setRemove<V>(Set<V> current, V v) => {
         for (final c in current)
           if (c != v) //
             c
       };
 
-  List<V> listAdd<V>(List<V> current, V v) => [...current, v];
-  List<V> listRemove<V>(List<V> current, V v) => [
+  static List<V> listAdd<V>(List<V> current, V v) => [...current, v];
+  static List<V> listRemove<V>(List<V> current, V v) => [
         for (final c in current)
           if (c != v) //
             c
       ];
 
-  Map<K, V> mapAdd<K, V>(Map<K, V> current, K k, V v) => {...current, k: v};
-  Map<K, V> mapRemove<K, V>(Map<K, V> current, K k) => {
+  static Map<K, V> mapAdd<K, V>(Map<K, V> current, K k, V v) => {...current, k: v};
+  static Map<K, V> mapRemove<K, V>(Map<K, V> current, K k) => {
         for (final c in current.entries)
           if (c.key != k) //
             c.key: c.value,
