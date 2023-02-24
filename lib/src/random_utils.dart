5393aeaa5b9c50bcf18f89df6c751795be355aa0
ltOgt <omnesiaorg@gmail.com>
2023-02-24 20:58:28 +0100

    typo + minBreadth

--- lib/src/random_utils.dart
+++ lib/src/random_utils.dart
@@ -5,37 +5,43 @@ class RandomUtils {
 
   Map<K, dynamic> randomMap<K>({
     required int maxDepth,
-    required int maxBredth,
+    required int maxBreadth,
+    int minBreadth = 1,
     required K rootKey,
     required K Function(K parent, int posInParent, int parentChildCount, int depth) getKey,
-  }) =>
-      _randomMap(
-        depth: 0,
-        maxDepth: maxDepth,
-        maxBredth: maxBredth,
-        parentKey: rootKey,
-        posInParent: 0,
-        parentChildCount: 1,
-        getKey: getKey,
-      );
+  }) {
+    assert(minBreadth <= maxBreadth);
+    return _randomMap(
+      depth: 0,
+      maxDepth: maxDepth,
+      maxBreadth: maxBreadth,
+      minBreadth: minBreadth,
+      parentKey: rootKey,
+      posInParent: 0,
+      parentChildCount: 1,
+      getKey: getKey,
+    );
+  }
 
   Map<K, dynamic> _randomMap<K>({
     required int depth,
     required int maxDepth,
-    required int maxBredth,
+    required int maxBreadth,
+    required int minBreadth,
     required K parentKey,
     required int posInParent,
     required int parentChildCount,
     required K Function(K parent, int posInParent, int parentChildCount, int depth) getKey,
   }) =>
       {
-        for (int i = 0, len = random.nextInt(maxBredth - 1) + 1; i < len; i++) //
+        for (int i = 0, len = random.nextInt(maxBreadth - minBreadth) + minBreadth; i < len; i++) //
           getKey(parentKey, i, len, depth): maxDepth == depth
               ? {}
               : _randomMap(
                   depth: depth + 1,
                   maxDepth: maxDepth,
-                  maxBredth: maxBredth,
+                  maxBreadth: maxBreadth,
+                  minBreadth: minBreadth,
                   parentKey: getKey(parentKey, i, len, depth),
                   posInParent: i,
                   parentChildCount: len,
