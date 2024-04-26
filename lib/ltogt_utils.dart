f8b18611076398f738946089ff3d10c7f9bc421c
ltOgt <omnesiaorg@gmail.com>
2024-04-27 01:35:48 +0200

    one to many

--- lib/ltogt_utils.dart
+++ lib/ltogt_utils.dart
@@ -24,6 +24,7 @@ export 'src/map/n_way_lookup.dart';
 export 'src/map/invert_map.dart';
 export 'src/map/read_only_map.dart';
 export 'src/map/put_if_absent_async.dart';
+export 'src/map/one_to_many.dart';
 
 export 'src/file/file_helper.dart';
 
---
+++ lib/src/map/one_to_many.dart
@@ -0,0 +1,45 @@
+// TODO needs testing lol
+
+import '../use.dart';
+import '../read_only.dart';
+
+/// Manages two maps in conjunction.
+///
+/// - [_oneToMany] maps :one: instance of [O] to a [Set] of :many: [M].
+/// - [_back] maps a single [M] back to the one [O].
+class OneToMany<O, M> {
+  final Map<O, Set<M>> _oneToMany = {};
+  final Map<M, O> _back = {};
+
+  Set<M>? getAllForOne(O one) => _oneToMany[one]?.use((set) => set.readOnly);
+  O? getOneForOneOfMany(M oneOfMany) => _back[oneOfMany];
+
+  void add(O one, M oneOfMany) {
+    _oneToMany.putIfAbsent(one, () => {}).add(oneOfMany);
+    _back[oneOfMany] = one;
+  }
+
+  void addAll(O one, Set<M> many) {
+    _oneToMany.putIfAbsent(one, () => {}).addAll(many);
+    for (final m in many) {
+      _back[m] = one;
+    }
+  }
+
+  void remove(O one) {
+    if (!_oneToMany.containsKey(one)) return;
+
+    for (final m in _oneToMany[one]!) {
+      _back.remove(m);
+    }
+    _oneToMany.remove(one);
+  }
+
+  void removeOneOfMany(M oneOfMany) {
+    if (!_back.containsKey(oneOfMany)) return;
+
+    final one = _back.remove(oneOfMany);
+    assert(_oneToMany.keys.contains(one));
+    _oneToMany[one]!.remove(oneOfMany);
+  }
+}
